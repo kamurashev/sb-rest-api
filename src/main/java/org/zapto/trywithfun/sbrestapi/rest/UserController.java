0f2ba7dabcf58982594206a62bb28415cfe9a2b0
kamurashev <kamurashev@gmail.com>
2021-01-17 23:49:20 +0200

    added SpringBoot actuator monitoring

--- src/main/java/org/zapto/trywithfun/sbrestapi/rest/UserController.java
+++ src/main/java/org/zapto/trywithfun/sbrestapi/rest/UserController.java
@@ -1,6 +1,10 @@
 package org.zapto.trywithfun.sbrestapi.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -25,31 +29,36 @@ public class UserController {
     @AdminOnly
     @GetMapping
     @ApiPageable
+    @Cacheable(value = "users_list_cache")
     public List<ApplicationUserDVO> list(@ApiIgnore Pageable pageable) {
         return applicationUserService.listView(pageable);
     }
 
     @AdminAndOwner
     @GetMapping("{login}")
+    @Cacheable(value = "users_cache")
     public ApplicationUserDVO get(@PathVariable String login) {
         return applicationUserService.getByLoginView(login);
     }
 
     @AdminOnly
     @PostMapping
+    @CachePut(cacheNames = "users_cache")
+    @Caching(put = {@CachePut(value = "users_cache")}, evict = {@CacheEvict(value = "users_list_cache", allEntries = true)})
     public ApplicationUserDVO create(@RequestBody @Validated({Create.class}) ApplicationUser user) {
         return applicationUserService.create(user);
     }
 
     @AdminAndOwner
     @PutMapping("{login}")
-    public ApplicationUserDVO update(@PathVariable String login,
-                        @RequestBody @Validated ApplicationUser user) {
+    @Caching(put = {@CachePut(value = "users_cache")}, evict = {@CacheEvict(value = "users_list_cache", allEntries = true)})
+    public ApplicationUserDVO update(@PathVariable String login, @RequestBody @Validated ApplicationUser user) {
         return applicationUserService.update(login, user);
     }
 
     @AdminAndOwner
     @DeleteMapping("{login}")
+    @Caching(evict = {@CacheEvict(value = "users_cache"),@CacheEvict(value = "users_list_cache", allEntries = true)})
     public void delete(@PathVariable String login) {
         applicationUserService.delete(login);
     }
